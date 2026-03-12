82dc313d1896dd98df89d3db5bad026dd8c2c1d1
hossamcharfi21-cloud <hossamcharfi21@gmail.com>
2026-03-12 07:07:41 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="refresh" content="0; url=https://evoiy.com/ref/bsklw">
-</head>
-<body>
-    <p>جاري تحويلك إلى الموقع...</p>
-</body>
-</html>
